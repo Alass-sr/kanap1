08ba8f4e0054676fd5d076ea5c21c8ae113c0144
Alass-sr <sarralass88@gmail.com>
2022-08-23 15:56:39 +0200

    nouveau repository

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function setCanap(kanap) {
+    let newLink = document.createElement("a");
+    newLink.href = `./product.html?id=${kanap._id}` 
+  
+    let newArticle = document.createElement("article");
+    let newImg = document.createElement("img");
+    newImg.src = kanap.imageUrl;
+    newImg.alt = kanap.altTxt;
+  
+    let newH3 = document.createElement("h3");
+    newH3.className = "productName";
+    newH3.textContent = kanap.name;
+  
+    let newP = document.createElement("p");
+    newP.className = "productDescription";
+    newP.textContent = kanap.description;
+  
+    // liaisons des éléments.
+  
+    newLink.appendChild(newArticle);
+    newArticle.appendChild(newImg);
+    newArticle.appendChild(newH3);
+    newArticle.appendChild(newP);
+  
+    return newLink;
+  }
+  
+  async function getProducts() {
+    await fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+  
+        let sectionProduits = document.getElementById("items");
+        for (let kanap of data) {
+          let myLink = setCanap(kanap);
+          sectionProduits.appendChild(myLink);
+        }
+      });
+  }
+  
+  getProducts();
